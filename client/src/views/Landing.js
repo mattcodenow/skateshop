65a8bad35c9d493a928db2e09969ec9e25038c67
Matthew Stewart <matthew.stewart.mi@gmail.com>
2023-07-18 11:34:38 -0700

    place landing placeholder image

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -1,7 +1,10 @@
+import { Image } from 'react-bootstrap'
+import skaters from '../images/skateboarders1.jpg'
+
 const Landing = () => {
   return (
     <div className="Landing">
-      Landing
+      <Image src={skaters} fluid />
     </div>
   )
 }
