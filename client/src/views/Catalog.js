5e208d407bc303b627705061e20dceb3761f554f
Matthew Stewart <matthew.stewart.mi@gmail.com>
2023-07-18 18:27:44 -0700

    implement axios get hook for products

--- client/src/views/Catalog.js
+++ client/src/views/Catalog.js
@@ -1,7 +1,47 @@
+import { useMemo } from 'react'
+import {
+  Container,
+  Col,
+  Row,
+  Card
+} from 'react-bootstrap'
+import useAxiosGet from '../hooks/useAxiosGet'
+import shortid from 'shortid'
+
 const Catalog = () => {
+  const { data, error, loaded } = useAxiosGet('http://localhost:3001/products')
+  
+  const results = useMemo(() => {
+    return data || []
+  }, [data])
+  
+  const listItems = results.map((result) => {
+    return (
+      <div
+        key={shortid.generate()}
+      >
+        {result.title}
+      </div>
+    )
+  })
+
   return (
     <div className="Catalog">
-      Catalog
+      <Container fluid>
+        <Row>
+          <Col md={4}>
+            <Card className="mt-3">
+              <Card.Header>Catalog</Card.Header>
+              <Card.Body>
+                <Card.Text>Browse available products.</Card.Text>
+                {!loaded && <Card.Text>Loading...</Card.Text>}
+                {error && <Card.Text>{error}</Card.Text>}
+                {loaded && <>{listItems}</>}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
