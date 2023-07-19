ac4076b53649699e61610cddef82be5390d9bb6d
Matthew Stewart <matthew.stewart.mi@gmail.com>
2023-07-18 18:26:49 -0700

    add products endpoint with mock products

--- server/server.js
+++ server/server.js
@@ -8,6 +8,33 @@ const port = process.env.API_PORT || 3001
 
 app.use(cors())
 
+/**
+ * Mock products to be replaced with DB.
+ */
+const mockProducts = [
+  {
+    id: 'a1b2c3d4',
+    categoryID: 'A',
+    title: 'Skateboard A',
+    description: 'Mockup generic skateboard A.',
+    price: 4999
+  },
+  {
+    id: 'a2b3c4d5',
+    categoryID: 'B',
+    title: 'Skateboard B',
+    description: 'Mockup generic skateboard B.',
+    price: 5999
+  }
+]
+
+/**
+ * Get all products.
+ */
+app.get('/products', (req, res) => {
+  res.json(mockProducts)
+})
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the API.'})
 })
